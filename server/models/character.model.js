e3498ec4c916d89f0f0a8fda5148ace0799328f6
Isaac <47983542+forbiddensandwich@users.noreply.github.com>
2024-05-05 17:43:58 -0400

    added server folder & files

---
+++ server/models/character.model.js
@@ -0,0 +1,27 @@
+import { Schema, model } from 'mongoose';
+
+const characterSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+        minLength: [2, 'Name must be at leat 2 characetrs'],
+        maxLength: [20, "Name can't be more than 20 characters"]
+    },
+    class: {
+        type: String,
+        required: [true, 'Class is required'],
+        minLength: [3, 'Class must be at least 3 characters'],
+        maxLength: [20, 'Class must be less than 20 characters']
+    },
+    race: {
+        type: String,
+        required: [true, 'Race is required'],
+        minLength: [3, 'Race must be at least 3 characters long'],
+        maxLength: [20, 'Race can only be 20 characters maximum']
+    },
+    gender: {
+        type: String,
+    }
+}, { timestamps: true });
+const Character = model('Character', characterSchema);
+export default Character;
