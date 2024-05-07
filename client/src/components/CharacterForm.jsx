a9db0da0cda8713d0e544bdf88d0a102e1a32b94
Morgan Ruiz-Demaree <120768738+mruizdem@users.noreply.github.com>
2024-05-06 20:09:30 -0500

    add create, update, view and form components

--- client/src/components/CharacterForm.jsx
+++ client/src/components/CharacterForm.jsx
@@ -1,20 +1,81 @@
 import React, { useState } from "react";
+import charData from "../data.js";
 
 const CharacterForm = (props) => {
+	const usableData = charData;
 	return (
 		<>
 			<form>
 				<div>
 					<label>Name:</label>
-					<input className="form-input" type="text" />
+					<input className="form-input mb-3" type="text" />
+				</div>
+				<div className="flex gap-3">
+					<div className="w-1/2">
+						<label>Race:</label>
+						<select name="race" id="race" className="form-input mb-3">
+							<option value="select" disabled selected>
+								--Select One--
+							</option>
+							{usableData.races.map((race, index) => (
+								<option key={index} value={race}>
+									{race}
+								</option>
+							))}
+						</select>
+					</div>
+					<div className="w-1/2">
+						<label>Class:</label>
+						<select name="charClass" id="charClass" className="form-input mb-3">
+							<option value="select" disabled selected>
+								--Select One--
+							</option>
+							{usableData.classes.map((charClass, index) => (
+								<option key={index} value={charClass}>
+									{charClass}
+								</option>
+							))}
+						</select>
+					</div>
+				</div>
+				<div className="flex gap-3">
+					<div className="w-1/2">
+						<label>Factions:</label>
+						<select name="faction" id="faction" className="form-input mb-3">
+							<option value="select" disabled selected>
+								--Select One--
+							</option>
+							{usableData.factions.map((faction, index) => (
+								<option key={index} value={faction}>
+									{faction}
+								</option>
+							))}
+						</select>
+					</div>
+					<div className="w-1/2">
+						<label>Weapons:</label>
+						<select name="weapon" id="weapon" className="form-input mb-3">
+							<option value="select" disabled selected>
+								--Select One--
+							</option>
+							{usableData.weapons.map((weapon, index) => (
+								<option key={index} value={weapon}>
+									{weapon}
+								</option>
+							))}
+						</select>
+					</div>
 				</div>
 				<div>
-					<label>Name:</label>
-					<input className="form-input" type="text" />
+					<label>Description:</label>
+					<input className="form-input mb-3" type="text" />
 				</div>
 				<div>
-					<label>Name:</label>
-					<input className="form-input" type="text" />
+					<label>Backstory:</label>
+					<input className="form-input mb-3" type="text" />
+				</div>
+				<div className="w-3/4 mx-auto">
+					<button className="btn-warning w-full">Submit</button>
 				</div>
 			</form>
 		</>
