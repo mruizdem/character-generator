704c3c0470778540657d14f468f2ba0d0b5c72da
Morgan Ruiz-Demaree <120768738+mruizdem@users.noreply.github.com>
2024-05-08 19:50:18 -0500

    backend accomidations for new features

--- client/src/data.js
+++ client/src/data.js
@@ -1,5 +1,6 @@
 const chardata = {
     races: ["Orc", "Elf", "Human", "Dwarf", "Wolfkin", "Kitsune", "Fairy"],
+    gender: ["Male", "Female", "Neither"],
     classes: ["Ranger", "Sorcerer", "Mage", "Bard", "Rogue", "Barbarian", "Paladin", "Gunslinger", "Monk", "Cleric"],
     factions: ["Empire", "Underground", "Forest Folk", "Neutral", "Hoard"],
     weapons: ["Axe", "Rapier", "Dagger", "Longbow", "Lance", "Gauntlets", "Greatsword", "Mace", "Warhammer", "Tomahawk", "Shuriken", "Blowgun", "Crossbow", "Flail", "Nunchuks", "Flintlock Pistols"],
